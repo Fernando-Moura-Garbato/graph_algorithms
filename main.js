def8c4330586cbced5b485ebc46ead39f74ddded
Fernando Moura Garbato <fernandogarbato@gmail.com>
2024-10-10 18:01:32 -0300

    email and onedrive search ready, but not optimized

--- main.js
+++ main.js
@@ -68,33 +68,85 @@ const graphClient = Client.initWithMiddleware({authProvider});
 //Obtendo um valor específico
 //await graphClient.api('/users/suporte02@grupounus.com.br/messages').get().then( (resposta) => {console.log(resposta.value[5].subject)})
 
-async function folderSearch(user, folderId, counter, search){
+
+//This function grabs a user, a folder ID and a search term, creates an API call and passes the result through an algorithm to count how many
+//times the search term appears on file names. It also recursively searches through folders within that folder, and in both cases, works with
+//data pagination. The repeatNext() function is declared within merely for organization.
+async function folderSearch(user, folderId, search){
+    let counter = 0;
     let folder = await graphClient.api('users/' + `${user}` + '/drive/items/' + `${folderId}` + '/children').get()
     for(let i = 0; i < folder.value.length; i++){
         if(folder.value[i].name.includes(search)){
             counter++;
         }
         if('folder' in folder.value[i]){
-            folderSearch(user, folder.value[i].id, counter, search)
+            counter = counter + await folderSearch(user, folder.value[i].id, search)
+        }
+        async function repeatNext(folder, search){
+                let counterNext = 0;
+                let chamada = await graphClient.api(folder['@odata.nextLink']).get();
+                for(let i = 0; i < chamada.value.length; i++){
+                    if(chamada.value[i].name.includes(search)){
+                        counterNext++;
+                    }
+                    if('folder' in chamada.value[i]){
+                        counterNext = counterNext + await folderSearch(user, chamada.value[i].id, search)
+                    }
+                    if(i === chamada.value.length - 1 && chamada['@odata.nextLink']){
+                        counterNext = counterNext + await repeatNext(chamada['@odata.nextLink'], search)
+                    }            
+                }
+                return counterNext;
+        }
+        if(i === folder.value.length - 1 && folder['@odata.nextLink']){
+        counter = counter + await repeatNext(folder, search)
         }
     }
+    return counter;
+}
+//This function looks within each message body preview in search for invites, and then 
+async function emailSearch(call, user){
+    let counter = {csv:0, docx:0, xlsx:0};
+    for(let i = 0; i < call.value.length; i++){
+        if(call.value[i].bodyPreview.includes('invited you to edit a file')){
+            let messageWithHtml = await graphClient.api('users/' + `${user}` + '/messages/' + `${call.value[i].id}`).get();
+            if(messageWithHtml.body.content.includes('csv')){
+                counter.csv++
+            }
+            if(messageWithHtml.body.content.includes('docx')){
+                counter.docx++
+            }
+            if(messageWithHtml.body.content.includes('xlsx')){
+                counter.xlsx++
+            }
+        }
+    }
+    if(call['@odata.nextLink']){
+        let nextCall = await graphClient.api(call['@odata.nextLink']).get();
+        let nextSearch = await emailSearch(nextCall, user);
+        counter.csv = counter.csv + emailSearch.csv
+        counter.docx = counter.docx + emailSearch.docx
+        counter.xlsx = counter.xlsx + emailSearch.xlsx
+    }
+    return counter;
 }
 
-console.log(await graphClient.api('users/suporte02@grupounus.com.br/drive/root/children').get())
+let call1 = await graphClient.api('users/suporte02@grupounus.com.br/messages').get();
+console.log(await emailSearch(call1, 'suporte02@grupounus.com.br'));
 
 
-async function officeSearch(clientVal) {
-    for(let i = 0; i < clientVal.value.length; i++){
-        let usuario = clientVal.value[i].userPrincipalName;
-        await handle.writeFile(clientVal.value[i].displayName + ';');
-        
-        await graphClient.api('users/suporte02@grupounus.com.br/drive/root/children').get()
+
+async function officeSearch(usuarios) {
+    for(let i = 0; i < usuarios.value.length; i++){
+        let usuario = usuarios.value[i].userPrincipalName;
+        await handle.writeFile(usuarios.value[i].displayName + ';');
 
     }
 }
 
 
 let handle = await fs.open('C:/Users/fernando.garbato/Desktop/graph_demo/teste.txt', 'w')
+
 //Paginação de dados
 // try{
 //     let resposta = await graphClient.api('/users').get();
